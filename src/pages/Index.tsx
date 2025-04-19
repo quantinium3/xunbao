00e181723b27932b7026606cf8da7c63af0c29c9
quantinium03 <vaibhavvx331@gmail.com>
2025-04-20 03:31:43 +0530

    changed font and stuff

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -20,7 +20,7 @@ const Index = () => {
                     </Link>
                 </div>
             </div>
-            <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 text-white my-5">
+            <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 text-white my-5 text-center">
                 Made by Manan
             </div>
         </>
