865e9d5c083bc4336fc29a167e43bb13dcfacb39
quantinium3 <quant@quantinium.dev>
2025-12-28 23:44:39 +0530

    cors and userRouter

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,16 +1,20 @@
 import { Hono } from 'hono'
 import { auth } from "./lib/auth";
 import { cors } from "hono/cors";
+import { userRouter } from "./routes/user";
 
 const app = new Hono()
 
 app
 	.use('/api/*', cors({
-		origin: '*' // NOTE:change to your domain
+		origin: ['http://localhost:5173'],
+		allowHeaders: ['Content-Type', 'Authorization'],
+		credentials: true,
 	}))
 	.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw))
 	.get('/', (c) => {
 		return c.text('Hello Hono!')
 	})
+	.route("/api/v1/user", userRouter)
 
 export default app
