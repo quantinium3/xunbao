7b842d6d68b9ce5be25f39d388988d4849192b8a
quantinium3 <quant@quantinium.dev>
2025-12-31 01:28:30 +0530

    fake caret and dash

---
+++ front/src/components/caret.tsx
@@ -0,0 +1,8 @@
+export function FakeCaret() {
+	return (
+		<div className="absolute pointer-events-none inset-0 flex items-center justify-center animate-caret-blink">
+			<div className="w-px h-8 bg-white" />
+		</div>
+	)
+}
+
---
+++ front/src/components/dash.tsx
@@ -0,0 +1,8 @@
+export function FakeDash() {
+	return (
+		<div className="flex w-10 justify-center items-center">
+			<div className="w-3 h-1 rounded-full bg-border bg-white" />
+		</div>
+	)
+}
+
