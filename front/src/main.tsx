4c5c498d4cc8f561bfdbee7df92aa108a4a1ea7f
quantinium3 <quant@quantinium.dev>
2025-12-31 02:41:44 +0530

    wtf who did it

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { routeTree } from './routeTree.gen'
+import './styles.css'
 
 const router = createRouter({ routeTree })
 
