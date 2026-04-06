51fa521bf858d3cc64e50670868bf761bc0e6f90
quantinium3 <quant@quantinium.dev>
2026-04-07 01:33:48 +0530

    chore: new migrations

---
+++ back/drizzle/0003_real_loa.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "user" ADD COLUMN "roll_number" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "college" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "branch" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "phone_number" text NOT NULL;
---
+++ back/drizzle/0004_fixed_longshot.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "user" ALTER COLUMN "roll_number" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "user" ALTER COLUMN "college" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "user" ALTER COLUMN "branch" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "user" ALTER COLUMN "phone_number" DROP NOT NULL;
