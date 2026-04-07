417fdbdf4b7fe936be0893bbded5f17310393ad0
quantinium3 <quant@quantinium.dev>
2026-04-08 01:31:10 +0530

    chore: matched additional types from backend

--- front/src/lib/auth-client.ts
+++ front/src/lib/auth-client.ts
@@ -1,7 +1,25 @@
 import { createAuthClient } from "better-auth/react"
-import { emailOTPClient } from "better-auth/client/plugins"
+import { inferAdditionalFields } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
 	baseURL: import.meta.env.VITE_API_URL,
-	plugins: [emailOTPClient()]
+	plugins: [inferAdditionalFields({
+		user: {
+			roll_number: {
+				type: "string"
+			},
+			college: {
+				type: "string",
+			},
+			branch: {
+				type: "string",
+			},
+			phone_number: {
+				type: "string",
+			},
+			is_onboarding_complete: {
+				type: "boolean",
+			}
+		}
+	})]
 })
