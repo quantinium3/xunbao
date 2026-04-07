774e555ad9686072c1196983192dd270bea571be
quantinium3 <quant@quantinium.dev>
2026-04-08 01:10:47 +0530

    chore: added button backdrop and images around it

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -8,15 +8,25 @@ export const Route = createFileRoute('/')({
 function Index() {
 	const nav = useNavigate();
 	return (
-		<div className="flex justify-center items-center h-screen flex-col font-press-start-2p space-y-5">
-			<h1 className="bg-gradient-to-r from-orange-500 via-white to-blue-500 bg-clip-text text-transparent text-5xl font-press-start-2p text-center">Welcome to Xunbao</h1>
-			<div className="flex gap-4">
-				<button className="btn" onClick={() => nav({ to: "/play" })}>Play</button>
+		<div className="flex justify-center h-screen lg:h-[66vh] flex-col space-y-2 px-4 pt-10">
+			<h1 className="text-yellow-50 text-5xl sm:text-6xl md:text-[clamp(80px,15vw,130px)] font-enchanted-land text-center [text-shadow:1px_1px_0_#000,-1px_-1px_0_#000,1px_-1px_0_#000,-1px_1px_0_#000]">
+				Welcome to Xunbao
+			</h1>
+			<img src="/assets/border.png" alt="" className="w-40 md:w-125 mx-auto -mt-2 md:-mt-5 sepia hue-rotate-30" />
+			<div className="flex gap-6 flex-col items-center mt-4">
 				<button
-					className="btn bg-gradient-to-r from-yellow-600 to-orange-600 hover:from-yellow-500 hover:to-orange-500"
+					className="group relative text-2xl sm:text-3xl md:text-5xl text-yellow-50 transition-all duration-300 hover:scale-103 font-bold [text-shadow:1px_1px_0_#000,-1px_-1px_0_#000,1px_-1px_0_#000,-1px_1px_0_#000] backdrop-blur-xl"
+					onClick={() => nav({ to: "/play" })}
+				>
+					<img src="/assets/button-outline.png" alt="" className="w-32 md:w-48 group-hover:scale-110 transition-transform sepia-[0.3] hue-rotate-30 group-hover:sepia-[0.5] group-hover:hue-rotate-40" />
+					<span className="absolute inset-0 flex items-center justify-center group-hover:text-yellow-100 transition-colors">Play</span>
+				</button>
+				<button
+					className="group relative text-xl sm:text-2xl md:text-5xl text-yellow-50 transition-all duration-300 hover:scale-103 font-bold [text-shadow:1px_1px_0_#000,-1px_-1px_0_#000,1px_-1px_0_#000,-1px_1px_0_#000] backdrop-blur-xl"
 					onClick={() => nav({ to: "/leaderboard" })}
 				>
-					Leaderboard
+					<img src="/assets/button-outline.png" alt="" className="w-40 md:w-56 group-hover:scale-110 transition-transform sepia-[0.3] hue-rotate-30 group-hover:sepia-[0.5] group-hover:hue-rotate-40deg" />
+					<span className="absolute inset-0 flex items-center justify-center group-hover:text-yellow-100 transition-colors">Leaderboard</span>
 				</button>
 			</div>
 		</div>
