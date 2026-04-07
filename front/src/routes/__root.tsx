905cc1ddd8d929b082b86244dce19e590b49e51e
quantinium3 <quant@quantinium.dev>
2026-04-08 01:08:08 +0530

    feat: added the gif in background

--- front/src/routes/__root.tsx
+++ front/src/routes/__root.tsx
@@ -6,6 +6,23 @@ import { Toaster } from 'react-hot-toast'
 
 const RootLayout = () => (
 	<div style={{ position: 'relative', width: '100vw', minHeight: '100vh' }}>
+		<div
+			style={{
+				position: 'fixed',
+				top: 0,
+				left: 0,
+				width: '100%',
+				height: '100%',
+				backgroundImage: 'url(/input.gif)',
+				backgroundSize: 'cover',
+				backgroundPosition: 'center',
+				zIndex: -2,
+			}}
+		/>
+		<img src="/assets/corner.png" alt="" className="fixed top-4 left-4 w-15 sm:w-20 md:w-25 z-0 invert sepia hue-rotate-30" />
+		<img src="/assets/corner.png" alt="" className="fixed top-4 right-4 w-15 sm:w-20 z-0 md:w-25 invert sepia hue-rotate-30 -scale-x-100" />
+		<img src="/assets/corner.png" alt="" className="fixed bottom-4 left-4 w-15 sm:w-20 z-0 md:w-25 invert sepia hue-rotate-30 -scale-y-100" />
+		<img src="/assets/corner.png" alt="" className="fixed bottom-4 right-4 w-15 sm:w-20 z-0 md:w-25 invert sepia hue-rotate-30 -scale-100" />
 		<Canvas
 			style={{
 				position: 'fixed',
@@ -13,7 +30,6 @@ const RootLayout = () => (
 				left: 0,
 				width: '100%',
 				height: '100%',
-				background: '#000000',
 				zIndex: -1,
 			}}
 			camera={{ position: [0, 0, 30], fov: 75 }}
@@ -21,7 +37,7 @@ const RootLayout = () => (
 			<ambientLight intensity={0.1} />
 			<Stars />
 		</Canvas>
-		<Toaster />
+		<Toaster toastOptions={{ style: { fontFamily: 'system-ui, sans-serif' } }} />
 		<Outlet />
 		<TanStackRouterDevtools />
 	</div>
