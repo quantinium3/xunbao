04d343c0308e07df1fa7da6966f7fda051d700bd
quantinium3 <quant@quantinium.dev>
2026-04-08 18:33:21 +0530

    chore: replaced better auth with clerk and removed some deps

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,12 +1,10 @@
 import 'dotenv/config';
 import { Hono } from 'hono'
-import { auth } from "./lib/auth";
 import { cors } from "hono/cors";
-import { userRouter } from "./routes/user";
 import { quizRouter } from "./routes/quiz";
 import { runMigrations } from "./lib/db/migrate";
-
-await runMigrations();
+import { clerkMiddleware } from '@clerk/hono';
+import webhookRouter from './routes/webhook';
 
 const corsOrigins = process.env.CORS_ORIGIN?.split(',').map(o => o.trim()) ?? ['http://localhost:5173'];
 
@@ -18,11 +16,12 @@ app
 		allowHeaders: ['Content-Type', 'Authorization'],
 		credentials: true,
 	}))
-	.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw))
+
+	.use('*', clerkMiddleware())
 	.get('/', (c) => {
 		return c.text('Hello Hono!')
 	})
-	.route("/api/v1/user", userRouter)
 	.route("/api/v1/quiz", quizRouter)
+	.route("/api/v1/webhook", webhookRouter)
 
 export default app
