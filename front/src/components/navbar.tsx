4649d8048d9e1a88698285cb9cf3e429b7d2eae7
quantinium3 <quant@quantinium.dev>
2026-04-08 18:34:30 +0530

    feat: changed navbar to user clerk component

--- front/src/components/navbar.tsx
+++ front/src/components/navbar.tsx
@@ -1,19 +1,18 @@
-import UserProfileDropdown from "./user-profile-dropdown";
 import { Link } from "@tanstack/react-router";
-import { authClient } from "../lib/auth-client";
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+
 
 export default function Navbar() {
-	const session = authClient.useSession();
 	return (
-		<div className="flex justify-between items-center p-2 mx-13 pt-11 font-bold z-10 font-enchanted-land">
-			<h1 className="text-xl sm:text-3xl text-yellow-50 hover:underline underline-offset-4"><Link to="/">XUNBAO</Link></h1>{" "}
-			{session.data ? (
-				<UserProfileDropdown />
-			) : (
-				<Link to="/sign-in" className="text-[10px] sm:text-sm border-2 border-white px-2 sm:px-3 py-1">
-					Sign In
-				</Link>
-			)}
+		<div className="flex justify-between items-center p-2 mx-13 pt-11 font-bold z-10 font-sans">
+			<h1 className="text-xl sm:text-3xl text-yellow-50 hover:underline underline-offset-4 font-enchanted-land"><Link to="/">XUNBAO</Link></h1>{" "}
+			<Show when="signed-out">
+				<SignInButton />
+				<SignUpButton />
+			</Show>
+			<Show when="signed-in">
+				<UserButton />
+			</Show>
 		</div>
 	);
 }
